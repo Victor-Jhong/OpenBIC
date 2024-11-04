9a252af07fb615b0611e02e853bb944254479100
victor <Victor.Jhong@quantatw.com>
2024-11-04 20:41:54 +0800

    support_log

--- meta-facebook/minerva-ag/src/shell/plat_shell.c
+++ meta-facebook/minerva-ag/src/shell/plat_shell.c
@@ -14,9 +14,71 @@
  * limitations under the License.
  */
 
+#include <stdlib.h>
 #include "plat_sensor_polling_shell.h"
+#include "plat_log.h"
+#include <stdio.h>
 #include "cpld_shell.h"
 #include "plat_pldm_fw_version_shell.h"
+
+void cmd_set_event(const struct shell *shell, size_t argc, char **argv)
+{
+	if (argc != 3) {
+		shell_warn(shell, "Help: test log set_event <num> <status>");
+		return;
+	}
+
+	int num = strtol(argv[1], NULL, 16);
+	int status = strtol(argv[2], NULL, 16);
+
+	error_log_event(num, status);
+
+	return;
+}
+
+void cmd_log_dump(const struct shell *shell, size_t argc, char **argv)
+{
+	if (argc != 3) {
+		shell_warn(shell, "Help: test log log_dump <num> <status>");
+		return;
+	}
+
+	int cmd_size = strtol(argv[1], NULL, 16);
+	int order = strtol(argv[2], NULL, 16);
+
+
+	uint8_t log_data[128] = {0};
+	plat_log_read(log_data, cmd_size, order);
+
+	shell_hexdump(shell, log_data, sizeof(uint8_t) * cmd_size);
+
+	return;
+}
+
+void cmd_log_clear(const struct shell *shell, size_t argc, char **argv)
+{
+	if (argc != 1) {
+		shell_warn(shell, "Help: test log log_clear");
+		return;
+	}
+
+	k_msleep(1000);
+
+	plat_clear_log();
+	shell_print(shell, "plat_clear_log finished!");
+
+
+	return;
+}
+
+
+SHELL_STATIC_SUBCMD_SET_CREATE(
+	sub_plat_log_cmd,
+	SHELL_CMD(set_event, NULL, "set_event", cmd_set_event),
+	SHELL_CMD(dump, NULL, "log_dump", cmd_log_dump),
+	SHELL_CMD(clear, NULL, "log_clear", cmd_log_clear),
+	SHELL_SUBCMD_SET_END);
+
 
 /* Sub-command Level 1 of command test */
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_test_cmds,
@@ -25,6 +87,8 @@ SHELL_STATIC_SUBCMD_SET_CREATE(sub_test_cmds,
 			       SHELL_CMD(cpld, &sub_cpld_cmd, "cpld command", NULL),
 			       SHELL_CMD(get_fw_version, &sub_get_fw_version_cmd,
 					 "get fw version command", NULL),
+ 			       SHELL_CMD(log, &sub_plat_log_cmd,
+					 "platform log command", NULL),
 			       SHELL_SUBCMD_SET_END);
 
 /* Root of command test */
