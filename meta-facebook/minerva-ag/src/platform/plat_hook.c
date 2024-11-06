b10eb05a6fb1dd1d9c3d008d810462e08cfcfc8f
victor <Victor.Jhong@quantatw.com>
2024-11-06 08:47:16 +0800

    add_vr_mutex_V1

--- meta-facebook/minerva-ag/src/platform/plat_hook.c
+++ meta-facebook/minerva-ag/src/platform/plat_hook.c
@@ -27,9 +27,55 @@
 
 LOG_MODULE_REGISTER(plat_hook);
 
+#define VR_MUTEX_LOCK_TIMEOUT_MS 1000
+
+static struct k_mutex vr_mutex[VR_MAX_NUM];
+
 vr_pre_proc_arg vr_pre_read_args[] = {
-	[0] = { 0x0 },
-	[1] = { 0x1 },
+	// mutex, vr_page
+	// P0V85
+	[0] = { vr_mutex, 0x0 },
+	[1] = { vr_mutex, 0x1 },
+
+	// P0V75_CH_N
+	[2] = { vr_mutex + 1, 0x0 },
+	[3] = { vr_mutex + 1, 0x1 },
+
+	// P0V75_CH_S
+	[4] = { vr_mutex + 2, 0x0 },
+	[5] = { vr_mutex + 2, 0x1 },
+
+	// P0V75_TRVDD_ZONEA
+	[6] = { vr_mutex + 3, 0x0 },
+	[7] = { vr_mutex + 3, 0x1 },
+
+	// P0V75_TRVDD_ZONEB
+	[8] = { vr_mutex + 4, 0x0 },
+	[9] = { vr_mutex + 4, 0x1 },
+
+	// P1V1_VDDC_HBM0_HBM2_HBM4
+	[10] = { vr_mutex + 5, 0x0 },
+	[11] = { vr_mutex + 5, 0x1 },
+
+	// P0V9_TRVDD_ZONEA
+	[12] = { vr_mutex + 6, 0x0 },
+	[13] = { vr_mutex + 6, 0x1 },
+
+	// P0V9_TRVDD_ZONEB
+	[14] = { vr_mutex + 7, 0x0 },
+	[15] = { vr_mutex + 7, 0x1 },
+
+	// P1V1_VDDC_HBM1_HBM3_HBM5
+	[16] = { vr_mutex + 8, 0x0 },
+	[17] = { vr_mutex + 8, 0x1 },
+
+	// VDDA_PCIE
+	[18] = { vr_mutex + 9, 0x0 },
+	[19] = { vr_mutex + 9, 0x1 },
+
+	// OSFP_P3V3
+	[20] = { vr_mutex + 10, 0x0 },
+	[21] = { vr_mutex + 10, 0x1 }
 };
 
 mp2971_init_arg mp2971_init_args[] = {
@@ -40,6 +86,16 @@ isl69259_init_arg isl69259_init_args[] = {
 	[0] = { .vout_scale_enable = true, .vout_scale = (499 / 798.8) },
 };
 
+void *vr_mutex_get(enum VR_INDEX_E vr_index)
+{
+	if (vr_index >= VR_MAX_NUM) {
+		LOG_ERR("vr_mutex_get, invalid vr_index %d", vr_index);
+		return NULL;
+	}
+
+	return vr_mutex + vr_index;
+}
+
 bool pre_vr_read(sensor_cfg *cfg, void *args)
 {
 	CHECK_NULL_ARG_WITH_RETURN(cfg, false);
@@ -49,6 +105,15 @@ bool pre_vr_read(sensor_cfg *cfg, void *args)
 	uint8_t retry = 5;
 	I2C_MSG msg;
 
+	/* mutex lock */
+	if (pre_proc_args->mutex) {
+		LOG_DBG("sen %x, mutex lock %p", cfg->num, pre_proc_args->mutex);
+		if (k_mutex_lock(pre_proc_args->mutex, K_MSEC(VR_MUTEX_LOCK_TIMEOUT_MS))) {
+			LOG_ERR("pre_vr_read, mutex lock fail");
+			return false;
+		}
+	}
+
 	/* set page */
 	msg.bus = cfg->port;
 	msg.target_addr = cfg->target_addr;
@@ -56,9 +121,30 @@ bool pre_vr_read(sensor_cfg *cfg, void *args)
 	msg.data[0] = 0x00;
 	msg.data[1] = pre_proc_args->vr_page;
 	if (i2c_master_write(&msg, retry)) {
+		k_mutex_unlock(pre_proc_args->mutex);
 		LOG_ERR("pre_vr_read, set page fail");
 		return false;
 	}
+	return true;
+}
+
+bool post_vr_read(sensor_cfg *cfg, void *args, int *reading)
+{
+	CHECK_NULL_ARG_WITH_RETURN(cfg, false);
+	CHECK_NULL_ARG_WITH_RETURN(args, false);
+	ARG_UNUSED(reading);
+
+	vr_pre_proc_arg *pre_proc_args = (vr_pre_proc_arg *)args;
+		
+	/* mutex unlock */
+	if (pre_proc_args->mutex) {
+		LOG_DBG("sen %x, mutex unlock %p", cfg->num, pre_proc_args->mutex);
+		if (k_mutex_unlock(pre_proc_args->mutex)) {
+			LOG_ERR("post_vr_read, mutex unlock fail");
+			return false;
+		}
+	}
+
 	return true;
 }
 
@@ -69,4 +155,12 @@ bool is_mb_dc_on()
    * 0 -> power off
    */
 	return gpio_get(RST_ATH_PWR_ON_PLD_R1_N);
+}
+
+void vr_mutex_init(void)
+{
+	for (uint8_t i = 0; i < ARRAY_SIZE(vr_mutex); i++) {
+		k_mutex_init(vr_mutex + i);
+		LOG_DBG("vr_mutex[%d] %p init", i, vr_mutex + i);
+	}
 }
