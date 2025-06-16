6c6cbf92731a1fbc5b8aaed85cf9743cb8d49844
victor <Victor.Jhong@quantatw.com>
2025-06-16 15:52:36 +0800

    change_all_nic_i2c_to_100K_2024_36.E1

--- meta-facebook/gt-cc/src/platform/plat_version.h
+++ meta-facebook/gt-cc/src/platform/plat_version.h
@@ -41,7 +41,7 @@
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x24
 #define BIC_FW_WEEK 0x36
-#define BIC_FW_VER 0xE0
+#define BIC_FW_VER 0xE1
 #define BIC_FW_platform_0 0x47 // char: G
 #define BIC_FW_platform_1 0x54 // char: T
 #define BIC_FW_platform_2 0x00 // char: '\0'
